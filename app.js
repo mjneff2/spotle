cb26ebaa1504003d8b1604216fa76c396fbae1bb
mjneff2 <mjneff922@gmail.com>
2022-03-28 21:39:14 -0500

    Added middleware for error handling and logging, added search endpoint

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const express = require('express') // Express web server framework
 const cors = require('cors')
 const axios = require('axios')
+const middleware = require('./utils/middleware')
+const logger = require('./utils/logger')
+require('express-async-errors')
 require('dotenv').config()
 
 const app = express()
 app.use(cors())
 app.use(express.json())
+app.use(middleware.requestLogger)
 
 const client_id = '8f9a74e7f1e047f5b7bc91dd53752e5d' // Your client id
 const client_secret = process.env.CLIENT_SECRET // Your secret
@@ -34,42 +38,33 @@ app.post('/login', async (req, res) => {
     let state = params.get('state')
     console.log(code, state)
     if (state !== null) {
-        try {
-            const result = await axios.post('https://accounts.spotify.com/api/token',
-                new URLSearchParams({
-                    grant_type: 'authorization_code',
-                    code,
-                    redirect_uri
-                }).toString()
-                , {
-                    headers: {
-                        'Authorization': 'Basic ' + (Buffer.from(client_id + ':' + client_secret).toString('base64')),
-                        'Content-Type': 'application/x-www-form-urlencoded'
-                    }
-                })
-            res.json({
-                access_token: result.data.access_token,
-                refresh_token: result.data.refresh_token
+        const result = await axios.post('https://accounts.spotify.com/api/token',
+            new URLSearchParams({
+                grant_type: 'authorization_code',
+                code,
+                redirect_uri
+            }).toString()
+            , {
+                headers: {
+                    'Authorization': 'Basic ' + (Buffer.from(client_id + ':' + client_secret).toString('base64')),
+                    'Content-Type': 'application/x-www-form-urlencoded'
+                }
             })
-        } catch (error) {
-            console.log(error.response.data)
-            res.json({error})
-        }
+        res.json({
+            access_token: result.data.access_token,
+            refresh_token: result.data.refresh_token
+        })
     }
 })
 
 app.post('/loggedin', async (req, res) => {
     const access_token = req.body.access_token
-    try {
-        const result = await axios.get('https://api.spotify.com/v1/me', {
-            headers: {
-                'Authorization': 'Bearer ' + access_token
-            }
-        })
-        return res.json(result.data)
-    } catch (error) {
-        return res.json(error.response.data)
-    }
+    const result = await axios.get('https://api.spotify.com/v1/me', {
+        headers: {
+            'Authorization': 'Bearer ' + access_token
+        }
+    })
+    return res.json(result.data)
 })
 
 app.get('/callback', async (req, res) => {
@@ -95,21 +90,39 @@ app.get('/callback', async (req, res) => {
     }
 })
 
-app.get('/toptracks', async (req,res) => {
+app.post('/track', async (req,res) => {
+    const access_token = req.body.access_token
     if (access_token === null) {
-        res.redirect('/login')
+        res.sendStatus(401)
     }
     const result = await axios.get('https://api.spotify.com/v1/me/top/tracks', {
         params: {
             limit: 50,
-            time_range: 'long_term'
+            time_range: 'long_term',
         },
         headers: {
             'Authorization': 'Bearer ' + access_token
         }
     })
-    res.send(result.data)
+    const track = result.data.items[Math.floor(Math.random() * result.data.items.length)]
+    res.send(track)
 })
+
+app.post('/search', async (req,res) => {
+    const query = req.body.query
+    const access_token = req.body.access_token
+    const result = await axios.get('https://api.spotify.com/v1/search', {
+        params: {
+            q: query,
+            type: 'track',
+            access_token
+        }
+    })
+    res.json(result.data)
+})
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
 app.listen(8888, () => {
     console.log('Listening on 8888')
